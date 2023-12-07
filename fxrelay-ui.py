ab4211a20c519ec2e15f4bfabc4a4a761d9b2f58
Hg <dev@indigo.re>
2023-12-07 14:09:21 +0100

    initial code import

---
+++ fxrelay-ui.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python3
+
+from dataclasses import dataclass
+import os
+
+from requests import Session
+
+from rich.text import Text
+from textual import log
+from textual.app import App, ComposeResult
+from textual.binding import Binding
+from textual.screen import ModalScreen
+from textual.widgets import DataTable, Input
+
+
+@dataclass
+class Column:
+    label: str
+    json_key: str | None
+    editable: bool = False
+
+    def format(self, value) -> str:
+        return str(value)
+
+    def sortkey(self, value):
+        return value
+
+
+class BoolColumn(Column):
+    def format(self, value) -> str:
+        if value:
+            return "✅"
+        else:
+            return " "
+
+
+class IntColumn(Column):
+    def sortkey(self, value):
+        return int(value)
+
+
+COLS = [
+    Column("Description", "description", editable=True),
+    Column("E-mail address", "full_address"),
+    IntColumn("ID", "id"),
+    BoolColumn("Enabled?", "enabled", editable=True),
+    BoolColumn("Block?", "block_list_emails", editable=True), # TODO merge columns
+    Column("Created at", "created_at"),
+    IntColumn("#Forwarded", "num_forwarded"),
+    IntColumn("#Blocked", "num_blocked"),
+    # Column("#Replied", "num_replied"),
+    # Column("#Spam", "num_spam"),
+]
+
+
+class FxRelay:
+    def __init__(self, token):
+        self.token = token
+        self.session = Session()
+        self.session.headers["Authorization"] = f"Token {token}"
+
+    def list_entries(self):
+        response = self.session.get("https://relay.firefox.com/api/v1/relayaddresses/")
+        response.raise_for_status()
+        return response.json()
+
+    def new_entry(self):
+        response = self.session.post(
+            "https://relay.firefox.com/api/v1/relayaddresses/",
+            json={},
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def edit_entry(self, id, changes):
+        log(changes)
+        return
+        response = self.session.patch(
+            f"https://relay.firefox.com/api/v1/relayaddresses/{id}",
+            json={},
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def delete_entry(self, id):
+        response = self.session.delete(f"https://relay.firefox.com/api/v1/relayaddresses/{id}")
+        response.raise_for_status()
+        return response.json()
+
+
+class Table(DataTable):
+    BINDINGS = [
+        ("T", "toggle_cell", "toogle"),
+    ]
+
+    [
+        Binding("/", "enter_search"),
+        Binding("n", "search_next"),
+        Binding("shift+n", "search_prev"),
+        Binding("Y", "copy_clipboard_cell"),
+        Binding("delete", "delete_row"),
+        Binding("ctrl+s", "save_changes"),
+    ]
+
+    def toggle_cell(self):
+        self.exit(1)
+
+
+class InputScreen(ModalScreen):
+    def __init__(self, value):
+        super().__init__()
+        self.init_value = value
+
+    def compose(self):
+        yield Input(value=self.init_value)
+
+    def on_input_submitted(self, message):
+        self.dismiss(message.value)
+
+
+class TableApp(App):
+    BINDINGS = [
+        Binding("(", "sort_asc_col"),
+        Binding(")", "sort_desc_col"),
+        Binding("ctrl+n", "new_row"),
+        Binding("e", "edit_cell"),
+        ("t", "toggle_cell", "blah"),
+    ]
+    # TODO search
+    # TODO edit input
+    # TODO edit bool
+    # TODO patches? [id, key] = value
+    # TODO error handling
+
+    def __init__(self, client):
+        super().__init__()
+        self.client = client
+        self.columns = {}
+        self.entries = {}
+        self.changes = {}
+
+    def compose(self) -> ComposeResult:
+        yield Table()
+
+    @property
+    def table(self):
+        # idiomatic?
+        return self.query_one(DataTable)
+
+    @property
+    def cursor_key(self):
+        return self.table.coordinate_to_cell_key(self.table.cursor_coordinate)
+
+    def action_new_row(self):
+        entry = self.client.new_entry()
+        self.entries[entry["id"]] = entry
+        self.table.add_row(*(col.format(jentry[col.json_key]) for col in COLS), key=jentry["id"])
+
+    def action_delete_row(self):
+        key = self.cursor_key.row_key
+        #self.client.delete_entry(key)
+        self.table.remove_row(key)
+        del self.entries[key]
+
+    def refresh_entries(self):
+        self.entries = {str(jentry["id"]): jentry for jentry in self.client.list_entries()}
+        self.table.clear()
+        for jentry in self.entries.values():
+            self.table.add_row(*(col.format(jentry[col.json_key]) for col in COLS), key=str(jentry["id"]))
+
+    def action_sort_asc_col(self):
+        col_key = self.cursor_key.column_key
+        self.table.sort(col_key, key=self.columns[col_key].sortkey)
+
+    def action_sort_desc_col(self):
+        col_key = self.cursor_key.column_key
+        self.table.sort(col_key, key=self.columns[col_key].sortkey, reverse=True)
+
+    def action_edit_cell(self):
+        col_key = self.cursor_key.column_key
+        row_key = self.cursor_key.row_key
+        column = self.columns[col_key]
+        if not column.editable:
+            return
+        current_value = self.entries[row_key][column.json_key]
+
+        def on_dismiss(value):
+            if value is None:
+                return
+            self.perform_edit(row_key, column, value)
+
+        self.push_screen(InputScreen(current_value), on_dismiss)
+
+    def perform_edit(self, row_key, column, value):
+        self.client.edit_entry(row_key, {column.json_key: value})
+
+    def on_mount(self) -> None:
+        for col in COLS:
+            col_key = self.table.add_column(col.label)
+            self.columns[col_key] = col
+        self.refresh_entries()
+
+# TODO async workers for client
+# TODO httpx
+
+
+if __name__ == "__main__":
+    client = FxRelay(os.environ["FXRELAY_TOKEN"])
+
+    app = TableApp(client)
+    app.run()
